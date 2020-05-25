c611b450a2e07e04a27fe3815f5124d458942af4
interestcoding <sylarxc@gmail.com>
2020-05-25 17:56:13 +0800

    read mark 1.5

---
+++ 1.1.py
@@ -0,0 +1,65 @@
+import collections
+from random import choice
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
+
+
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
+
+
+if __name__ == "__main__":
+    print('-' * 80)
+    beer_card = Card('7', 'diamonds')
+    print(beer_card)
+
+    print('-' * 80)
+    deck = FrenchDeck()
+    print(len(deck))
+
+    print('-' * 80)
+    print(deck[0])
+    print(deck[-1])
+
+    print('-' * 80)
+    print(choice(deck))
+    print(choice(deck))
+    print(choice(deck))
+
+    print('-' * 80)
+    print(deck[:3])
+    print(deck[12::13])
+
+    print('-' * 80)
+    for card in deck:
+        print(card)
+
+    print('-' * 80)
+    for card in reversed(deck):
+        print(card)
+
+    print('-' * 80)
+    print(Card('Q', 'hearts') in deck)
+    print(Card('7', 'bearts') in deck)
+
+    print('-' * 80)
+    for card in sorted(deck, key=spades_high):
+        print(card)
