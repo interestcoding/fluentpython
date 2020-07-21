e9e3caaa4e3804f10feb513342a44276debc65f8
interestcoding <sylarxc@gmail.com>
2020-07-21 16:57:44 +0800

    2.3 part1

---
+++ 2.3.py
@@ -0,0 +1,38 @@
+import os
+
+# 2.3.1
+lax_coordinates = (33.9425, -118.408056)
+city, year, pop, chg, area = ('Tokyo', 2003, 32450, 0.66, 8014)
+traveler_ids = [('USA', '31195855'), ('BRA', 'CE342567'), ('ESP', 'XDA205856')]
+for passport in sorted(traveler_ids):
+    print('%s/%s' % passport)
+
+for country, _ in traveler_ids:
+    print(country)
+
+# 2.3.2
+latitude, longitude = lax_coordinates
+print(latitude)
+print(longitude)
+
+print(divmod(20, 8))
+
+t = (20, 8)
+print(divmod(*t))
+
+quotient, remainder = divmod(*t)
+print((quotient, remainder))
+
+_, filename = os.path.split('/home/luciano/.ssh/idrsa.pub')
+print(filename)
+
+a, b, *rest = range(5)
+print((a, b, rest))
+
+a, b, *rest = range(3)
+print((a, b, rest))
+
+a, b, *rest = range(2)
+print((a, b, rest))
+
+# 2.3.3
